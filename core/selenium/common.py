3541a722ad25075b90526f09fd765bdb4fd8109a
Ramón Gavira <ramgavsan@alum.us.es>
2024-12-03 15:58:56 +0100

    am i seeding???

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,8 +9,21 @@ def initialize_driver():
     options = webdriver.ChromeOptions()
 
     # Initialise the browser using WebDriver Manager
-    service = Service(ChromeDriverManager().install())
+    # driver_path = ChromeDriverManager().install()
+    # chromediver_binary = os.path.join(os.path.dirname(driver_path), 'chromedriver')
+    # service = Service(chromediver_binary)
+    
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.binary_location = ('/home/ramon/Descargas/chrome-linux64 (1)/chrome-linux64/chrome')
+
+    os.getenv('SELENIUM_BINARY_LOCATION', '')
+
+        # Iniciar el driver de Chromium usando webdriver-manager
+    service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
+        
+    # driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 
